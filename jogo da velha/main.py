84552b136a44737ee9142a1937749c22885c019d
Vinicius L <81776878+Niussu@users.noreply.github.com>
2024-11-11 13:24:55 -0300

    Add files via upload

---
+++ jogo da velha/main.py
@@ -0,0 +1,76 @@
+from models.functions import *
+player1 = input("Digite o nome do jogador 1: ")
+print("Você será o X")
+player2 = input("Digite o nome do jogador 2: ")
+print("Você será o O")
+
+gameboard = create_matrix() 
+gui_gameboard = create_gui_matrix()
+
+i = 0
+for i in range(9):
+    print("-"*50)
+    
+    if i % 2 == 0: 
+        print(f'{player2}, sua vez: ')
+        
+        gui_gameboard_pass = update_gui_matrix(gameboard, gui_gameboard)
+        gui_gameboard = gui_gameboard_pass
+        show_gui_gameboard(gui_gameboard)
+
+        choice_line = int(input("Digite a linha onde desaja marcar: "))
+        choice_col = int(input("Digite a coluna onde deseja marcar: "))
+        
+
+        while is_marked(choice_line, choice_col, gameboard):
+            print("-"*50)
+            print(f'Linha {choice_line} e coluna {choice_col} já estão ocupados \nEscolha outra posição!!!')
+            choice_line = int(input("Digite a linha onde deseja marcar: "))
+            choice_col = int(input("Digite a coluna onde deseja marcar: "))
+
+        gameboard_pass = mark_matrix(choice_line, choice_col, gameboard, 2)
+        gameboard = gameboard_pass
+        condition = win_condition(gameboard)
+        if condition != 0:
+            if condition == 1:
+                print("-"*33)
+                print(f"| PARABÉNS {player1}, VOCÊ GANHOU!!! |")
+                print("-"*33)
+                break
+            else:
+                print("-"*33)
+                print(f"| PARABÉNS {player2}, VOCÊ GANHOU!!! |")
+                print("-"*33)
+                break
+        
+
+    else:#jogador 1
+        print(f'{player1}, sua vez: ')
+
+        gui_gameboard_pass = update_gui_matrix(gameboard, gui_gameboard)
+        gui_gameboard = gui_gameboard_pass
+        show_gui_gameboard(gui_gameboard)
+
+        choice_line = int(input("Digite a linha onde desaja marcar: "))
+        choice_col = int(input("Digite a coluna onde deseja marcar: "))
+
+        while is_marked(choice_line, choice_col, gameboard):
+            print("-"*50)
+            print(f'Linha {choice_line} e coluna {choice_col} já estão ocupados \nEscolha outra posição!!!')
+            choice_line = int(input("Digite a linha onde deseja marcar: "))
+            choice_col = int(input("Digite a coluna onde deseja marcar: "))
+            
+        gameboard_pass = mark_matrix(choice_line, choice_col, gameboard, 1)
+        gameboard = gameboard_pass
+        condition = win_condition(gameboard)
+        if condition != 0:
+            if condition == 1:
+                print("-"*33)
+                print(f"| PARABÉNS {player1}, VOCÊ GANHOU!!! |")
+                print("-"*33)
+                break
+            else:
+                print("-"*33)
+                print(f"| PARABÉNS {player2}, VOCÊ GANHOU!!! |")
+                print("-"*33)
+                break
---
+++ jogo da velha/models/functions.py
@@ -0,0 +1,65 @@
+def create_matrix():
+    matrix = []
+    for i in range(3):
+        line = []
+        for i in range(3):
+            line.append(0)
+        matrix.append(line)
+    return matrix
+
+def mark_matrix(line, col, gameboard, player):
+    gameboard[line][col] = player
+    return gameboard
+
+def is_marked(line, col, gameboard):
+    if gameboard[line][col] != 0:
+        return True
+    else:
+        return False
+
+def win_condition(gameboard):
+
+    for i in range(3):
+        if gameboard[i][0] == gameboard[i][1] == gameboard[i][2] and gameboard[i][0] != 0:
+            return gameboard[i][0]  
+
+    for i in range(3):
+        if gameboard[0][i] == gameboard[1][i] == gameboard[2][i] and gameboard[0][i] != 0:
+            return gameboard[0][i]  
+
+    if gameboard[0][0] == gameboard[1][1] == gameboard[2][2] and gameboard[0][0] != 0:
+        return gameboard[0][0] 
+
+    if gameboard[0][2] == gameboard[1][1] == gameboard[2][0] and gameboard[0][2] != 0:
+        return gameboard[0][2]  
+
+    return 0
+
+def create_gui_matrix():
+    matrix = []
+    for i in range(3):
+        line = []
+        for j in range(3):
+            line.append("-")
+        matrix.append(line)
+    return matrix
+            
+def update_gui_matrix(gameboard, gui_gameboard):
+    for i in range(3):
+        for j in range(3):
+            if gameboard[i][j] == 1:
+                gui_gameboard[i][j] = "X"
+            if gameboard[i][j] == 2:
+                gui_gameboard[i][j] = "O"
+    return gui_gameboard
+
+def show_gui_gameboard(gui_gameboard):
+    print("\t0\t1\t2")
+    #print("\t________________")
+    print("")
+    for i in range(3):
+        print(i, end='\t')
+        for j in range(3):
+            print(gui_gameboard[i][j], end='\t')
+        print("\n")
+    print("\n")
